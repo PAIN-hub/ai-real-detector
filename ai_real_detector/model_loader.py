acb9383c287bbea0257ddc741ff5762cb3f43aee
ƤȺIƝ <163692189+PAIN-hub@users.noreply.github.com>
2025-10-03 17:49:52 +0100

    Update model_loader.py

--- ai_real_detector/model_loader.py
+++ ai_real_detector/model_loader.py
@@ -1,7 +1,20 @@
-import tensorflow as tf
 import os
+import gdown
+from keras.models import load_model
 
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "../model/EfficientNet_model.h5")
+MODEL_DIR = os.path.join(os.path.dirname(__file__), "..", "model")
+MODEL_PATH = os.path.join(MODEL_DIR, "EfficientNet_model.h5")
 
-def load_model():
-    return tf.keras.models.load_model(MODEL_PATH)
+FILE_ID = "1b_X8kmWhXbOvNS0OaXOe_RwAaAgz42ZM"
+URL = f"https://drive.google.com/uc?id={FILE_ID}"
+
+def get_model():
+    """Ensure model is available and load it."""
+    os.makedirs(MODEL_DIR, exist_ok=True)
+
+    if not os.path.exists(MODEL_PATH):
+        print("Downloading EfficientNet model...")
+        gdown.download(URL, MODEL_PATH, quiet=False)
+
+    print("Model ready for inference!")
+    return load_model(MODEL_PATH)
